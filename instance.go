cc5338287274f3e66b15fc20e760be03fb207494
Andy Walker <andy@andy.dev>
2023-06-01 16:41:13 -0400

    Prettify auth a bit

--- instance.go
+++ instance.go
@@ -193,22 +193,30 @@ MESSAGELOOP:
 	return nil
 }
 
+const (
+	authPluginToken = "org.apache.pulsar.client.impl.auth.AuthenticationToken"
+	authPluginNone = ""
+)
+
+
 func (gi *goInstance) setupClient() error {
 	ic := gi.context.instanceConf
 
 	var authProvider pulsar.Authentication
 	switch ic.authPlugin {
-	case "":
-		authProvider, _ = pulsar.NewAuthentication("", "")
-	case "org.apache.pulsar.client.impl.auth.AuthenticationToken":
+	case authPluginToken:
 		switch {
 		case strings.HasPrefix(ic.authParams, "file://"):
 			authProvider = pulsar.NewAuthenticationTokenFromFile(ic.authParams[7:])
 		case strings.HasPrefix(ic.authParams, "token:"):
 			authProvider = pulsar.NewAuthenticationToken(ic.authParams[6:])
+		case ic.authParams == "" :
+			return fmt.Errorf("auth plugin %s given, but authParams is empty", authPluginToken)
 		default:
 			return fmt.Errorf(`unknown token format - expecting "file://" or "token:" prefix`)
 		}
+	case authPluginNone:
+		authProvider, _ = pulsar.NewAuthentication("", "")
 	default:
 		return fmt.Errorf("unknown auth provider: %s", ic.authPlugin)
 	}
